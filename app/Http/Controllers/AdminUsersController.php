485d50e9d0265aa05cc5f87c4bef794d731b914f
yalain03 <yalain9@gmail.com>
2017-07-31 08:21:11 -0500

    User creation page

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +17,9 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -27,6 +30,7 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
+        return view('admin.users.create');
     }
 
     /**
@@ -38,6 +42,7 @@ class AdminUsersController extends Controller
     public function store(Request $request)
     {
         //
+        return $request->all();
     }
 
     /**
@@ -60,6 +65,7 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+        return view('admin.users.edit');
     }
 
     /**
---
+++ resources/views/admin/users/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+        <div class="form-group">
+            {!! Form::label('name', 'Name:') !!}
+            {!! Form::text('name', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('email', 'Email:') !!}
+            {!! Form::email('email', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('role_id', 'Role:') !!}
+            {!! Form::text('role_id', null, ['class'=>'form-control']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('status', 'Status:') !!}
+            {!! Form::select('status', array(1 => 'Active', 0 => 'Not Active'), 0, ['class'=>'form-control']) !!}
+        </div>
+        <div class="hidden">
+            {!! csrf_token() !!}
+        </div>
+        <div class="form-group">
+            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+        </div>
+    {!! Form::close() !!}
+@endsection
